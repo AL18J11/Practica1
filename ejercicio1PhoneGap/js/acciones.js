b5feef05b6edf3d881c9df355cc0124d988bde3c
Luis Miguel Salvador <salvadorgtrc@gmail.co>
2013-03-20 08:00:20 -0600

    version 1.0

---
+++ ejercicio1PhoneGap/js/acciones.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+(document).ready(function(e) {
+alert ("documento listo")
+});
+document.addEventListener("deviceready",function(){
+	$('.btn').tap(function(){
+		var boton =(($(this).attr('class')).split
+		switch (boton)
+		{
+		case 'b1'
+		navigator.
+		break;
+		case 'b2'
+		break;
+		case 'b3'
+		break;
+		case 'b4'
+		break;
+		case 'b5'
+		break;
+		(' '))[1];
+		alert(boton)
+	});
+	},false);
+
+	
+	
