2550f4c7a49abf59678ab7e0ff4f8570a4a8c5d6
AL18J11 <salvadorgtrc@gmail.com>
2013-04-08 13:44:50 -0500

    Update acciones.js

--- ejercicio1PhoneGap/js/acciones.js
+++ ejercicio1PhoneGap/js/acciones.js
@@ -8,20 +8,32 @@ document.addEventListener("deviceready",function(){
 		switch (boton)
 		{
 		case 'b1'
-		navigator.
+		navigator.notification.beep(1);
 		break;
 		case 'b2'
+		navigator.notification.beep(2);
 		break;
 		case 'b3'
+		navigator.notification.beep(5);
 		break;
 		case 'b4'
+		navigator.notification.vibrate(500);
 		break;
 		case 'b5'
+		navigator.notification.vibrate(300);
+		   navigator.notification.vibrate(1000);
 		break;
-		(' '))[1];
-		alert(boton)
+		}
+	        });
+	$('derecha').swiperight(function(){
+		navigator.notification.alert("deslizame a la derecha", function(){},"practica 1","ACEPTAR"
 	});
 	},false);
+	    $('table td').eq(1).text(device.name);
+            $('table td').eq(3).text(device.cordova);
+            $('table td').eq(5).text(device.plataform);
+            $('table td').eq(7).text(device.version);
+            $('table td').eq(9).text(device.UUID);
 
 	
 	
